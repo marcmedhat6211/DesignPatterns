e5195691d822b43fe79ef1e25e2902eb1b464b91
marcmedhat6211 <marcmedhat6211@gmail.com>
2020-11-07 14:22:18 +0200

    Builder Design Pattern

---
+++ BuilderPattern/Builder.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designpatterns.BuilderPattern;
+
+/**
+ *
+ * @author marc
+ */
+public interface Builder {
+    public void setCarType(CarType type);
+    public void setSeats(int seats);
+    public void setEngine(Engine engine);
+    public void setTransmission(Transmission transmission);
+    public void setTripComputer(TripComputer tripComputer);
+    public void setGPSNavigator(GPSNavigator gpsNavigator);
+}
---
+++ BuilderPattern/GPSNavigator.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package designpatterns.BuilderPattern;
+
+/**
+ *
+ * @author marc
+ */
+public class GPSNavigator {
+    private String route;
+
+    public GPSNavigator() {
+        this.route = "221b, Baker Street, London  to Scotland Yard, 8-10 Broadway, London";
+    }
+
+    public GPSNavigator(String manualRoute) {
+        this.route = manualRoute;
+    }
+
+    public String getRoute() {
+        return route;
+    }
+}
